ab63c18a813fabc15754f7f5d8a79c09718d08b4
Andrew Mukhin <andreybwd@gmail.com>
2016-01-05 13:45:51 +0200

    Fixed

--- scripts/hockey.sh
+++ scripts/hockey.sh
@@ -4,4 +4,6 @@ pushd platforms/ios/
 
 xcodebuild clean -project "$XCODE_PROJECT" -configuration Release -alltargets
 
+xcodebuild archive -project "$XCODE_PROJECT" -scheme "$XCODE_SCHEME" -archivePath "$APPNAME"
+
 popd
